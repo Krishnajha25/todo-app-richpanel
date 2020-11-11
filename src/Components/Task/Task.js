47beda3313d34632769078e6efba21c79c590520
Krishnajha25 <krishnajha366@gmail.com>
2020-11-12 00:10:07 +0530

    New chnages

--- src/Components/Task/Task.js
+++ src/Components/Task/Task.js
@@ -1,13 +1,22 @@
 import React from 'react'
 import './Task.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faClock } from '@fortawesome/free-solid-svg-icons'
+
 
 
 function Task(props) {
 
     return (
         <div className="task__container">
-            <h3> {props.task} </h3>
-            <h5 className="date"> Due Fri, Aug 20 </h5>
+            <div className="left">
+                <h3> {props.task} </h3>
+                <h5 className="date"> Due Fri, Aug 20 </h5>
+            </div>
+            <div className="right">
+                <FontAwesomeIcon icon={faClock} />
+            </div>
+
         </div>
     )
 }
